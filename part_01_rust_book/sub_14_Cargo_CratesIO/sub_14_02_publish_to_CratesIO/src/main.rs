3783afe24e436a3dcaf4c27c84d38c63520193e1
Longistamina <dophamthelong03@gmail.com>
2026-08-13 11:51:15 +0900

    updated at 11:51:15 2026-08-13

---
+++ part_01_rust_book/sub_14_Cargo_CratesIO/sub_14_02_publish_to_CratesIO/src/main.rs
@@ -0,0 +1,8 @@
+use sub_14_02_publish_to_CratesIO::PrimaryColor;
+use sub_14_02_publish_to_CratesIO::mix;
+
+fn main() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
+}
