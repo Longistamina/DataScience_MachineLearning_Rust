af8d92b50e8eec57dfecaa7ffafa7b60f6c6f48f
Longistamina <dophamthelong03@gmail.com>
2026-08-26 12:55:33 +0900

    updated at 12:55:33 2026-08-26

--- part_01_rust_book/sub_15_SmartPointers_DeRef_Drop/src/bin/06_MemoryLeak_ReferenceCycles.rs
+++ part_01_rust_book/sub_15_SmartPointers_DeRef_Drop/src/bin/06_MemoryLeak_ReferenceCycles.rs
@@ -1,12 +1,24 @@
 /*
-"Memory leak" is the memory that is never cleaned up.
-=> causes system to overflow and crash.
+A memory leak is memory that is no longer useful to the program
+but is never released.
+
+If enough memory is leaked over time,
+the process or system may eventually run out of memory.
 
 Generally, Rust has rules to prevent memory leak,
 this is sometimes called "safe Rust".
 
-However, Rust does allow "unsafe Rust" via using ``Rc<T>`` and ``RefCell<T>``.
-In those cases, Rust cannot fully guarantee memory safe.
+Safe Rust prevents:
+- dangling references
+- use-after-free
+- double free
+- data races
+
+Safe Rust does NOT guarantee:
+- every allocation will eventually be freed
+
+Therefore ``Rc<RefCell<T>>`` reference cycles can leak memory
+without using unsafe Rust.
 
 One example of memory leak are Reference Cycles.
 Let's demo it here
@@ -31,48 +43,75 @@ impl<T> List<T> {
     }
 }
 
+// ============ //
+//    main()    //
+// ============ //
+
 fn main() {
     println!();
 
     let a = Rc::new(Cons(32.5, RefCell::new(Rc::new(Nil)))); // a = Cons(32.5, Nil)
-    println!("a initial rc count = {}", Rc::strong_count(&a));
-    println!("a next item = {:?}", a.tail());
+    println!("a initial rc count = {}", Rc::strong_count(&a)); // 1
+    println!("a next item = {:?}", a.tail()); // Some(RefCell { value: Nil })
 
     println!();
 
     let b = Rc::new(Cons(64.8, RefCell::new(Rc::clone(&a)))); // b = Cons(64.8, a) = Cons(64.8, Cons(32.5, Nil))
     println!("a rc count after b creation = {}", Rc::strong_count(&a)); // 2
     println!("b initial rc count = {}", Rc::strong_count(&b)); // 1
-    println!("b next item = {:?}", b.tail());
+    println!("b next item = {:?}", b.tail()); // Some(RefCell { value: Cons(32.5, RefCell { value: Nil }) })
 
     println!();
 
-    if let Some(link) = a.tail() { // ``link`` will be ``RefCell { value: Nil }`` (not ``Nil``)
+    if let Some(link) = a.tail() {
         *link.borrow_mut() = Rc::clone(&b);
-        // ``*link`` is ``Nil``
-        // so ``*link.borrow_mut()`` returns ``&mut Nil``
-        // then mutates this ``&mut Nil`` to ``Rc::clone(&b)``
+        // ``link`` is a reference to ``RefCell<Rc<List<T>>>``.
+        // ``link.borrow_mut()`` gives temporary mutable access to the Rc stored inside that RefCell -> ``RefMut<Rc<List<T>>>``
+        // ``*link.borrow_mut()`` dereference to strip away the ``RefMut`` and get the assignable ``Rc<List<T>>`` -> ``Rc<List<T>>``
+        // ``*link.borrow_mut() = Rc::clone(&b)`` -> mutates ``Rc<List<T>>`` into this ``Rc<&b>``
+        //
+        // This increases the strong_count of ``b`` by 1 => becomes 2
         // In short, we changed ``a`` from Cons(32.5, Nil) to Cons(32.5, b) -> results in a vicious cycle
     }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b)); // 2
+    println!("a rc count after changing a = {}", Rc::strong_count(&a)); // 2
+
     /*
     Originally, we have ``b -> a`` like this ``b = Cons(64.8, a) = Cons(64.8, Cons(32.5, Nil))``.
+    + a strong_count = 2
+    + b strong_count = 1
 
-    But after running this ``*link.borrow_mut() = Rc::clone(&b)``, we modifies the value ``Nil`` in a to b
+    After running this ``*link.borrow_mut() = Rc::clone(&b)``,
+    we modifies the value ``Nil`` inside ``a`` to become a ``Rc<T>`` pointer pointing to ``b``
     -> results in ``Cons(32.5, b)``.
+    + a strong_count = 2
+    + b strong_count = 2
 
-    But inside b, we also has ``b = Cons(64.8, a)``
+    What is the point of this?
+
+    When ``main()`` ends, local variables ``a`` and ``b`` are dropped,
+    so their strong_count drops by 1:
+    + a strong_count: 2 -> 1
+    + b strong_count: 2 -> 1
+
+    So, their strong_count never reaches 1. Why?
+
+    Because inside b, we also has ``b = Cons(64.8, a)``
     => this form an infinite reference cyles ``a -> b -> a -> b -> a -> ....``
+    => they will live forever
 
-    b ---> a
-    ^      |
-    |      v
-    <------
+    b --Rc--> a
+    ^         |
+    |         |
+    +---Rc----+
 
-    => this reference cyle makes the stack overflow
+    => this reference cyle makes both allocations leak
     */
 
-    println!("b rc count after changing a = {}", Rc::strong_count(&b));
-    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+    //----------------------//
+    // Make the LEAK happen //
+    //----------------------//
 
     // Uncomment the next line to see that we have a cycle; it will overflow the stack ().
     // println!("a next item = {:?}", a.tail());
--- part_01_rust_book/sub_15_SmartPointers_DeRef_Drop/src/bin/07_WeakT_RcDowngrade_prevent_MemoryLeak.rs
+++ part_01_rust_book/sub_15_SmartPointers_DeRef_Drop/src/bin/07_WeakT_RcDowngrade_prevent_MemoryLeak.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, non_snake_case)]
+
 /*
 In the previous file ``06_MemoryLeak_ReferenceCycles.rs``,
 we saw that reference cycles can lead to memory leak
@@ -28,3 +30,141 @@ it will increase the ``weak_count`` of ``Rc<T>`` instance by 1
 
 DIFFERENCE: ``weak_count`` does not need to be 0 for the ``Rc<T>`` to be cleaned up.
 */
+
+// =====================================================================================
+// 1. Demo ``Weak<T>``: create a tree data structure
+// =====================================================================================
+/*
+To best understand ``Weak<T>``, we will build a tree with nodes
+that know about their child nodes.
+
+Call that struct ``Node``.
+
+The flow should be:
++ parent --Rc--> child
++ child --Weak--> parent
+
+The parent owns the child, but the child does not own the parent.
+That means the child can still know who its parent is without keeping that parent alive forever
+(``child --Rc--> parent`` will keep this parent alive forever, making the stack overflow)
+*/
+
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>, // ``Rc<Node>`` to keep track how many pointers pointing to that child node
+                                      // ``Vec<Rc<Node>>`` to share the ownership with variables so that we can access each Node in the tree directly.
+                                      // ``RefCell<Vec<Rc<Node>>>`` to allow modifying which nodes are children of another node
+
+    parent: RefCell<Weak<Node>> // makes the child aware of its parent without keeping the parent alive forever
+                                // this increases ``weak_count``, leaves the ``strong_count`` the same
+                                // so, the ``strong_count`` of parent can reach 0 -> no more cycle
+}
+
+// ---------------------- //
+
+fn demo_WeakT() {
+    let leaf = Rc::new(Node { // create a ``leaf`` with value=3, empty children, and empty WEAK parent
+        value: 3,
+        children: RefCell::new(vec![]),
+        parent: RefCell::new(Weak::new()),
+    });
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade()); // None
+    /*
+    ``upgrade()`` explain:
+
+    Because the value that ``Weak<T>`` references might have been dropped,
+    to do anything with the value that a Weak<T> is pointing to you must make sure the value still exists.
+
+    To do so, we can call ``upgrade()`` method on a ``Weak<T>`` instance.
+    If the value is dropped or empty => ``None``
+    If the value exists              => ``Some(...)``
+    */
+
+    let branch = Rc::new( Node { // create a ``branch`` with value=5, children is ``leaf``, and empty WEAK parent
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]), // ``branch`` wil own ``leaf``, increasing strong_count of ``leaf`` by 1
+        parent: RefCell::new(Weak::new())
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    // ``leaf.parent`` is ``RefCell<Weak<T>>``
+    // ``leaf.parent.borrow_mut()`` is ``RefMut<Weak<T>>``
+    // ``*leaf.parent.borrow_mut()`` strips away the ``RefMut`` and returns the assignable ``Weak<T>``
+    // ``Rc::downgrade(&branch)`` create a ``Weak<Node>`` reference to ``branch`` (increase weak_count by 1, not strong_count)
+    // ``*leaf.parent.borrow_mut() = Rc::downgrade(&branch)`` mutates the ``Weak<T>`` into the ``Weak<&branch>``
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade()); // Some(...)
+}
+
+
+// =====================================================================================
+// 2. Visualizing Changes to ``strong_count`` and ``weak_count``
+// =====================================================================================
+/*
+Let’s look at how the ``strong_count`` and ``weak_count`` values of the ``Rc<Node>`` instances change
+by creating a new inner scope and moving the creation of branch into that scope.
+
+By doing so, we can see what happens when branch is created and then dropped when it goes out of scope.
+*/
+
+fn visualize_strong_weak_count() {
+    let leaf = Rc::new(Node { // Create a ``leaf`` here
+        value: 7,
+        children: RefCell::new(vec![]),
+        parent: RefCell::new(Weak::new())
+    });
+
+    println!( // Print the strong_count and weak_count before going to scope
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf), // 1
+        Rc::weak_count(&leaf) // 0
+    );
+
+    { // Go to inner scope
+        let branch = Rc::new(Node { // Create ``branch``
+            value: 12,
+            children: RefCell::new(vec![Rc::clone(&leaf)]), // ``branch`` owns the ``leaf``, increase strong_count of ``leaf`` by 1
+            parent: RefCell::new(Weak::new())
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch); // Create link ``branch`` to ``Weak<T>`` pointer of ``leaf``
+
+        println!( // print the counts of ``branch``
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch), // 1
+            Rc::weak_count(&branch), // 1
+        );
+
+        println!( // print the counts of ``leaf``
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf), // 2
+            Rc::weak_count(&leaf), // 0
+        );
+    } // Go out of inner scope, the ``branch`` is now dropped
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade()); // should be ``None``
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf), // 1
+        Rc::weak_count(&leaf), // 0
+    );
+}
+
+// ============ //
+//    main()    //
+// ============ //
+
+fn main() {
+    println!();
+
+    demo_WeakT();
+
+    println!("\n====================================================================================\n");
+
+    visualize_strong_weak_count();
+}
